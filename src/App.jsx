fbb5596eefceb09cb834309c0629a9b2498efd77
Breno Tardelli <brenot.dev@gmail.com>
2025-06-08 16:28:28 +0200

    styled css  has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "../src/ui/Button";
+import Input from "../src/ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: green;
+`;
+
+const StyledApp = styled.div`
+  background-color: orange;
+  padding: 20px;
+`;
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <StyledApp>
+        <GlobalStyles />
+        <H1>The Wild Oasis</H1>
+        <Input type="number" placeholder="Number of people" />
+        <Button onClick={() => alert("alooooooo")}>Check in</Button>
+      </StyledApp>
+    </>
+  );
 }
