4870b5014d41086e1d5b5931e4d3ff7959c87f6b
Breno Tardelli <brenot.dev@gmail.com>
2025-06-09 17:20:51 +0200

    the app layout   has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
-import React from "react";
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "../src/ui/Button";
-import Input from "../src/ui/Input";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: green;
-`;
-
-const StyledApp = styled.div`
-  background-color: orange;
-  padding: 20px;
-`;
-
-export default function App() {
+function App() {
   return (
     <>
-      <StyledApp>
-        <GlobalStyles />
-        <H1>The Wild Oasis</H1>
-        <Input type="number" placeholder="Number of people" />
-        <Button onClick={() => alert("alooooooo")}>Check in</Button>
-      </StyledApp>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
+
+export default App;
